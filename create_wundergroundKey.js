6a99df79a22d1f3e37af776d7c3258522f0b8ef9
geek <bbqbailey@gmail.com>
2015-12-16 14:46:50 -0500

    changed format and name, so deleting this element

--- create_wundergroundKey.js
+++
@@ -1,6 +0,0 @@
-var Datastore = require('nedb');
-var key_db = new Datastore( { filename: './myWundergroundKey.db', autoload: true});
-
-var myKey=" Place the key you obtained from wunderground.com in here. ";
-
-key_db.insert({'my_key':myKey});
