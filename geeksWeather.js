ee6dd00f61a0d690c657571e9dbed37b97f8f552
geek <bbqbailey@gmail.com>
2015-12-16 14:42:29 -0500

    adding capability to use city, state, zip, and station.  Also changed name from key db to info.db

--- geeksWeather.js
+++ geeksWeather.js
@@ -3,32 +3,42 @@ var Datastore    = require('nedb');
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
 
-var db           = new Datastore( { filename: './wunderground.db', autoload: true });
-var myKeyDB      = new Datastore( { filename: './myWundergroundKey.db', autoload: true });
+var db     = new Datastore( { filename: './wunderground.db', autoload: true });
+var wgInfo = new Datastore( { filename: './myWundergroundInfo.db', autoload: true });
 
 var id; //definefd in getData()
-var wunderground; //defined in getKey()
-var myKey=""; //defined in getKey()
+
+var wunderground; //defined in getWUInfo()
+var myKey=""; //defined in getWUInfo()
+var city="";
+var state="";
+var zip="";
+var station=""
+
 var emitter_weather={};
 
 
 //get your stored key you obtained from wunderground.com
-//If errors, then be sure you have added your key to file 'create_wundergroundKey.js'
-//then run the js via 'nodejs create_wundergroundKey.js' to create
-//your keyfile ./myWundergroundKey.db
+//If errors, then be sure you have added your key to file 'create_wundergroundInfo.js'
+//then run the js via 'nodejs create_wundergroundInfo.js' to create
+//your keyfile ./myWundergroundInfo.db
 //
-function getKey() {
-    console.log("getKey() entry");
-    myKeyDB.find({}, function(err, keyDoc) {
+function getWUInfo() {
+    console.log("getWUInfo() entry");
+    wgInfo.find({}, function(err, keyDoc) {
         if(err) {
-            console.log("getKey() exit on error");
+            console.log("getWUInfo() exit on error");
             return;
         }
         console.log("keyDoc: ", keyDoc[0]);
-        myKey=keyDoc[0].my_key;
+        myKey=keyDoc[0].myKey;
+        city=keyDoc[0].city;
+        state=keyDoc[0].state;
+        zip=keyDoc[0].zip;
+        station=keyDoc[0].station;
         console.log("Proceeding with key: ", myKey);
         wunderground = new Wunderground(myKey);
-        console.log("getKey() exit");
+        console.log("getWUInfo() exit");
         processWeatherData();
     });
 }
@@ -41,32 +51,35 @@ function createEmitterData(err, weather) {
         return;
     } else {
         console.log("createEmitterData() entry");
-        var observation_time    = weather.current_observation.observation_time; 
-        var temp_f              = weather.current_observation.temp_f;
-        var local_time          = weather.current_observation.local_time_rfc822; 
-        var local_epoch         = weather.current_observation.local_epoch;
-        var wind_mph            = weather.current_observation.wind_mph;
-        var wind_gust_mph       = weather.current_observation.wind_gust_mph;
-        var wind_string         = weather.current_observation.wind_string;
-        var wind_dir            = weather.current_observation.wind_dir;
-        var wind_degrees        = weather.current_observation.wind_degrees;
-        var relative_humidity   = weather.current_observation.relative_humidity; 
-        var pressure_mb         = weather.current_observation.pressure_mb;
-        var pressure_in         = weather.current_observation.pressure_in; 
-        var pressure_trend      = weather.current_observation.pressure_trend; 
-        var dewpoint_f          = weather.current_observation.dewpoint_f;
-        var feelslike_f         = weather.current_observation.feelslike_f;
-        var visibility_mi       = weather.current_observation.visibility_mi;
-        var precip_today_in     = weather.current_observation.precip_today_in;
-        var icon                = weather.current_observation.icon;
+        obs=weather.current_observation;
+        var observation_time    = obs.observation_time; 
+        var temp_f              = obs.temp_f;
+        var local_time          = obs.local_time_rfc822; 
+        var local_epoch         = obs.local_epoch;
+        var wind_mph            = obs.wind_mph;
+        var wind_gust_mph       = obs.wind_gust_mph;
+        var wind_string         = obs.wind_string;
+        var wind_dir            = obs.wind_dir;
+        var wind_degrees        = obs.wind_degrees;
+        var relative_humidity   = obs.relative_humidity; 
+        var pressure_mb         = obs.pressure_mb;
+        var pressure_in         = obs.pressure_in; 
+        var pressure_trend      = obs.pressure_trend; 
+        var dewpoint_f          = obs.dewpoint_f;
+        var feelslike_f         = obs.feelslike_f;
+        var visibility_mi       = obs.visibility_mi;
+        var precip_today_in     = obs.precip_today_in;
+        var icon                = obs.icon;
 
         emitter_weather = { 'observation_time': observation_time, 'temp_f': temp_f, 'local_time': local_time, 'local_epoch': local_epoch, 
             'wind_mph': wind_mph, 'wind_gust_mph': wind_gust_mph, 'wind_string': wind_string, 'wind_dir': wind_dir, 'wind_degrees': wind_degrees,
             'relative_humidity': relative_humidity, 'pressure_mb': pressure_mb, 'pressure_in': pressure_in, 'pressure_trend': pressure_trend,
-            'dewpoint_f': dewpoint_f, 'feelslike_f': feelslike_f, 'visibility_mi': visibility_mi, 'precip_today_in': precip_today_in, 'icon': icon }
+            'dewpoint_f': dewpoint_f, 'feelslike_f': feelslike_f, 'visibility_mi': visibility_mi, 'precip_today_in': precip_today_in, 'icon': icon,
+            'city': city, 'state': state, 'zip': zip, 'station': station 
+        }
 
         eventEmitter.emit('newWeatherData', emitter_weather);
-        console.log("createEmitterData() exit");
+        console.log("createEmitterData() exit; emitter_weather: ", emitter_weather);
     }
 }
 
@@ -90,7 +103,7 @@ function dbInsertWeatherData(err, weather) {
             console.log("temp_f: ", obs.temp_f);
             console.log();
        
-            local_epoch_val = parseInt(weather.current_observation.local_epoch);
+            local_epoch_val = parseInt(obs.local_epoch);
             db.update({ _id: id }, { $set: { 'local_epoch_val': local_epoch_val } });
         }
     });
@@ -101,14 +114,14 @@ function dbInsertWeatherData(err, weather) {
 //gets weather data and stores it
 function processWeatherData() {
     console.log("processWeatherData() entry");
-    wunderground.conditions().request('pws/q/pws:KGAALPHA23', dbInsertWeatherData);
+    wunderground.conditions().request('pws/q/pws:' + station, dbInsertWeatherData);
 
     console.log("processWeatherData() exit");
 }
 
 //=========================active program starts
 
-getKey();
+getWUInfo();
 
 var minute = 1000 * 60; //useful substitution 
 setInterval(processWeatherData, 5 * minute); //schedule next run
