d45e3a853cf56e2f1350af2600d979fd536858a7
geek <bbqbailey@gmail.com>
2016-01-14 11:15:24 -0500

    Too many changes to list.  Basically, complete refractoring of app.

--- geeksWeather.js
+++ geeksWeather.js
@@ -1,141 +1,118 @@
 var Wunderground = require('wundergroundnode');
 var Datastore    = require('nedb');
-var http         = require("http");
 var async        = require("async");
 var log4js       = require('log4js');
-var fs           = require('fs');
-var querystring  = require('querystring');
-var util        = require('util');
+var express      = require('express');
+var bodyParser   = require('body-parser');
+var app = express();
 
 var logger = log4js.getLogger();
-logger.setLevel('INFO'); //In order: ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL
+logger.setLevel('TRACE'); //In order: ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL
 
-var MAX_NON_REPORT_TIME_MINUTES=60;//amount of time that can elapse before we alarm on weather station down
-var MAX_NON_REPORT_TIME_SECS = MAX_NON_REPORT_TIME_MINUTES * 60; //amount of time, in seconds, to alarm on weather station down
-var MINUTE = 1000 * 60; //useful substitution 
-var MINUTES_UNTIL_GET_WEATHER_DATA = 5 * MINUTE; //determines when we request weather data from Wunderground.  Default is 5 * MINUTE
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const WUNDERGROUND_GET_TIME = 5 * MINUTE;
+//const WEATHER_SEND_TIME = WUNDERGROUND_GET_TIME - 1;
+const TIME_SEND_TIME = 1 * SECOND;
 
-var SHOW_DATA_ON_SERVER = true; //determines whether emitter data will be shown on server.
-
-var db     = new Datastore( { filename: './wunderground.db', autoload: true });
 var wgInfo = new Datastore( { filename: './myWundergroundInfo.db', autoload: true });
-
-var id; //definefd in getData()
-var wunderground; //defined in getAppInfo()
-var my_key=""; //defined in getAppInfo()
-var city="";
-var state="";
-var zip="";
-var station="";
-var myPort="";
-var myServer="";
-var server_time_now=new Date();
-
 var emitter_weather={};
+var connections=[];
+var wunderground; //defined in getAppInfo()
 var wunderground_weather={}; //contains weather info returned by Wunderground.com API
 
-function createAppServer(callback) {
-    //
-    //a useful web page that addresses handling forms without a framework is
-    //http://blog.frankgrimm.net/2010/11/howto-access-http-message-body-post-data-in-node-js/
-    //
-    logger.trace("createAppServer() entry");
 
-    http.createServer(function(request, response) {
-        var index = "./weatherServer";
-        var interval;
-        var temp_f = emitter_weather.temp_f;
-        var last_time="";
-        var weather;
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+//or app.use(bodyParser.json()) if urlencoded is incorrect;
 
-        var time_now_seconds = Math.floor(new Date().getTime()/1000);
-        if(time_now_seconds - emitter_weather.observation_epoch >= MAX_NON_REPORT_TIME_SECS ) {
-            logger.error("****************WEATHER STATION EXCEEDED NON-REPORTING TIME*****************");
-            logger.error("Switch to alternate weather station");
-        } else {
-            logger.debug("WEATHER STATION OK");
-        }
-        
-        logger.debug("server page requested is " + request.url);
-       
-        if(request.url === "/") {
-            filename = index; logger.debug("changing requested server page to filename " + filename);
-        } else {
-            filename = "." + request.url;
-            logger.debug("changed requested server page to filename " + filename);
-        }
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.set('views', __dirname + '/views');
 
-        if(filename === "./weatherServer") {
-            logger.trace("filename === '/.weatherServer' is true ");
-            response.writeHead(200, {"Content-type":"text/event-stream", "Cache-Control":"no-cache", "Connection":"keep-alive"});
-            response.write("retry: 10000\n");
-            response.write("event: connectime\n");
-            interval = setInterval(function() {
-                response.write("data: " + (new Date()) + "\n");
-                response.write("data: " + "temp_f: " + temp_f + "\n");
-                if(last_time !== emitter_weather.local_epoch) {
-                    last_time = emitter_weather.local_epoch;
-                    weather = JSON.stringify(emitter_weather);
-                    response.write("data: " + weather + "\n\n");
-                }
-            }, 1000);
-            request.connection.addListener("close", function() {
-                clearInterval(interval);
-            }, false);
-        } else if(request.url === "/configureGeeksWeather") {
-            //
-            //a useful web page that addresses handling forms without a framework is
-            //http://blog.frankgrimm.net/2010/11/howto-access-http-message-body-post-data-in-node-js/
-            //
-            logger.trace("request.url === configureGeeksWeather");
-            fs.readFile("configureGeeksWeather.html","binary", function(err, file) {
-                if(err) {
-                    response.writeHead(500, {"Content-Type": "text/plain"});
-                    response.write(err + "\n");
-                    response.end();
-                    return;
-                }
-                response.writeHead(200);
-                response.write(file, "binary");
-                response.end();
-            });
-        } else if(request.url === "/configurationFormHandler") {
-            logger.trace("request.url === configurationFormHandler");
-            logger.trace("request.url is ", request.url);
-            logger.debug("request.method: ", request.method); 
-            if(request.method.toLowerCase() == 'post') {
-                var fullBody = "";
-                logger.trace("---received POST method");
-                request.on('data', function(chunk) {
-                    fullBody += chunk.toString();
-                });
-
-                request.on('end', function() {
-                    response.writeHead(200, "OK", {'Content-Type':'text/html'});
-
-                    var decodedBody = querystring.parse(fullBody);
-                    response.write('<html><head><title>Post Data</title></head><body><pre>');
-                    response.write(util.inspect(decodedBody));
-                    response.write('</pre></body></html>');
-                    response.end();
-                });
-            } else {
-                response.writeHead(405, "Method not supported", {'Content-Type': 'text/html'});
-                response.end('<html><head><title>405 - Method not supported</title></head><body><H1>Method not supported.</h1></body</html>');
+app.get('/eventEngine' , function(req, res) {
+    logger.trace('app.get(/eventEngine) entry');
+    connections.push(res);
+    logger.debug("Added a connection; connections.length: " + connections.length);
+    res.writeHead(200, {
+        "Content-type":"text/event-stream", 
+        "Cache-Control":"no-cache", 
+        "Connection":"keep-alive"
+    });
+    sendWeather();
+    
+    req.on("close", function() {
+        logger.trace("req.on close");
+        var toRemove;
+        for(var i=0;i<connections.length; i++) {
+            if(connections[i] == res) {
+                toRemove=i;
+                break;
             }
- 
-        } else {
-            logger.warn("Requested server page " + request.url + " not a valid hosted page.");
         }
-    }).listen(myPort, myServer);
-    logger.info("listening on port " + myPort);
-    logger.info("listening on server " + myServer);
-    logger.trace("createAppServer() exit");
-    if(typeof callback === "function")
-        callback();
-}
-        
+        connections.splice(i,1);
+        logger.debug("Removed a connection; connections.length: " + connections.length);
+    });
+});
 
+app.get('/detail', function(req, res, next) { //renders __dirname/views/index.jade
+    logger.trace('app.get(/detail) entry');
+    res.render('detail');
+});
+
+
+app.listen(8080, function (err) {
+    logger.info('Express started on port 8080');
+});
+
+
+var wundergroundInterval = setInterval(getWeatherData, WUNDERGROUND_GET_TIME);
+//var weatherInterval = setInterval(sendWeather, WEATHER_SEND_TIME);
+var dateInterval = setInterval(sendTime, TIME_SEND_TIME);
+
+
+
+function sendWeather() {
+    logger.trace("sendWeather() entry");
+    var weatherJson = JSON.stringify(emitter_weather);
+    sendConnections(weatherJson, 'weather');
+}
+
+function sendTime() {
+    logger.trace("sendTime() entry");
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes  = time.getMinutes();
+    var seconds = time.getSeconds();
+    if(hours > 12) {
+        hours -= 12;
+    } else if (hours === 0) {
+        hours = 12;
+    }
+
+    if(minutes < 10)
+        minutes = "0"+ minutes;
+    if(seconds < 10)
+        seconds = "0" + seconds;
+
+    var time_data = hours + ":" + minutes + ":" + seconds;
+    
+    timeJson = JSON.stringify(time_data);
+    sendConnections(timeJson,'time');
+    logger.debug("Number of connections: " + connections.length);
+}
+
+function sendConnections(data, event) {
+    logger.trace("sendConnections() entry");
+    logger.trace("sendConnections() data: ", data);
+    logger.debug("sendConnections() event: ", event);
+    for(var i=0; i<connections.length; i++ ) {
+        connections[i].write('event: ' + event + '\n');
+        connections[i].write('data: ' + data + '\n\n');
+    }
+}    
+    
+    
 //get your stored key you obtained from wunderground.com
 //If errors, then be sure you have added your key to file 'create_wundergroundInfo.js'
 //then run the js via 'nodejs create_wundergroundInfo.js' to create
@@ -172,41 +149,15 @@ function getWeatherData(callback) {
     logger.trace("getWeatherData() exit");
     if (typeof callback === "function")
         callback();
-
+    sendWeather();    
 }
 
 function processWundergroundData(err, weather) {
     logger.trace("processWundergroundData() entry");
-    dbInsertWeatherData(err, weather);
+//    dbInsertWeatherData(err, weather);
     createEmitterData(err, weather);
-    showWundergroundData(err, weather);
+//    showWundergroundData(err, weather);
     logger.trace("processWundergroundData() exit");
-}
-
-
-//callback for Wunderground API usage.
-//param weather will contain results obtained from Wunderground
-function dbInsertWeatherData(err, weather) {
-    logger.trace("dbInsertWeatherData() entry");
-    logger.debug("weather: ", weather);
-
-    db.insert(weather, function(err, doc) {
-        if(err) {
-            logger.error("dbInsertWeatherData() error: ", err);
-            return;
-        } else {
-
-            id=doc._id;
-            logger.debug("db insert record ID: " + id);
-
-            obs=weather.current_observation;
-
-      
-            local_epoch_val = parseInt(obs.local_epoch);
-            db.update({ _id: id }, { $set: { 'local_epoch_val': local_epoch_val } });
-        }
-    });
-    logger.trace("dbInsertWeatherData() exit");
 }
 
 function createEmitterData(err, weather) {
@@ -215,15 +166,16 @@ function createEmitterData(err, weather) {
         return;
     } else {
         logger.trace("createEmitterData() entry");
-        obs=weather.current_observation;
+        var obs=weather.current_observation;
+        var forecastday=weather.forecast.simpleforecast.forecastday;
        
-        server_time_now     = new Date();
+        var server_time_now     = new Date();
         
         emitter_weather = { 'observation_epoch': obs.observation_epoch, 'observation_time': obs.observation_time, 'temp_f': obs.temp_f, 'local_time': obs.local_time_rfc822, 'local_epoch': obs.local_epoch, 
             'wind_mph': obs.wind_mph, 'wind_gust_mph': obs.wind_gust_mph, 'wind_string': obs.wind_string, 'wind_dir': obs.wind_dir, 'wind_degrees': obs.wind_degrees,
             'relative_humidity': obs.relative_humidity, 'pressure_mb': obs.pressure_mb, 'pressure_in': obs.pressure_in, 'pressure_trend': obs.pressure_trend,
             'dewpoint_f': obs.dewpoint_f, 'feelslike_f': obs.feelslike_f, 'visibility_mi': obs.visibility_mi, 'precip_1hr_in': obs.precip_1hr_in, 'precip_today_in': obs.precip_today_in, 'icon': obs.icon,
-            'city': city, 'state': state, 'zip': zip, 'station': station, 'server_time': server_time_now 
+            'city': city, 'state': state, 'zip': zip, 'station': station, 'server_time': server_time_now, 'forecast':forecastday
         }
 
         logger.debug("createEmitterData(); emitter_weather: ", emitter_weather);
@@ -231,61 +183,13 @@ function createEmitterData(err, weather) {
     logger.trace("createEmitterData() exit");
 }
 
-function showWundergroundData(err, weather) {
-    if(SHOW_DATA_ON_SERVER) {
-        logger.trace("showWundergroundData() entry");
-        var forecastday = weather.forecast.simpleforecast.forecastday;
-        if(logger.level.isLessThanOrEqualTo("DEBUG")) {
-            logger.debug("===========weather forecast start============ ");
 
-            forecastday.forEach(function(forecast) {
-                logger.debug(forecast);
-            });
-            logger.debug("===========weather forecast end============ ");
-            logger.debug();
-        }
+//async.series([getAppInfo, createAppServer, getWeatherData],function(err) {
+async.series([getAppInfo, getWeatherData, sendWeather], function(err) {
 
-        obs=weather.current_observation;
-
-        console.log("===================== Info ============================================");
-        console.log("Wunderground local_time: ", obs.local_time_rfc822);
-        console.log("Server time: ", server_time_now);
-        console.log("Station ", obs.observation_time);
-        console.log("Station: ", station);
-        console.log("City: ", obs.observation_location.city);
-        console.log("State: ", obs.observation_location.state);
-        console.log("Zip: ", obs.display_location.zip);
-        console.log("Latitude: ", obs.observation_location.latitude);
-        console.log("Longitude: ", obs.observation_location.longitude);
-        console.log("Station Elevation: ", obs.observation_location.elevation);
-        console.log();
-        console.log("===================== CURRENT WEATHER =================================");
-        console.log("temp_f: ", obs.temp_f);
-        console.log("wind: ", obs.wind_string);
-        console.log("icon: ", obs.icon);
-        console.log();
-        console.log("===================== FORECAST WEATHER ================================");
-        forecastday.forEach(function(forecast) {
-           console.log("Day: " + forecast.date.weekday );
-           console.log("High: " + forecast.high.fahrenheit);
-           console.log("Low: " + forecast.low.fahrenheit);
-           console.log("Conditions: " + forecast.conditions);
-           console.log("Wind Avg: " + forecast.avewind.mph + " Dir: " + forecast.avewind.dir);
-           console.log("Wind Max: " + forecast.maxwind.mph + " Dir: " + forecast.maxwind.dir);
-           console.log();
-        });
-
-        console.log();
-        logger.trace("showWundergroundData() exit");
-    }
-}
-
-
-async.series([getAppInfo, createAppServer, getWeatherData],function(err) {
     if(err) {
         logger.error("========================async.series error: ", err);
          return;
     }
 });
 
-setInterval(getWeatherData, MINUTES_UNTIL_GET_WEATHER_DATA); //schedule next run
