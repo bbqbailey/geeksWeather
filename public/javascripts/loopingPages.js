6149eba014440a4cf1c1fb4e585427b18897ceda
geek <bbqbailey@gmail.com>
2016-02-27 15:25:26 -0500

    Additional cleanup of display, added splash page.

--- public/javascripts/loopingPages.js
+++ public/javascripts/loopingPages.js
@@ -21,6 +21,7 @@ if(typeof DELAY === 'undefined') {
    console.log("loopingPages DELAY is: " + DELAY);
 }
 
+var imgHeight="980px"
 var docHeight = document.body.clientHeight;
 var docWidth  = document.body.clientWidth;
 
@@ -28,7 +29,6 @@ function iframeDidLoad() {
   alert('Done');
 }
 
-var imgHeight="980px"
 
 setInterval(function() {
   console.log("fired");
@@ -37,13 +37,11 @@ setInterval(function() {
 
 function buildHtml( displayObj ) {
   console.log("buildHtml() entry: displayObj: ", displayObj);
-  var htmlString = '<' + displayObj.displayType + ' src="' + displayObj.uri + '" height="' + displayObj.height + '">;';
+  var htmlString = '<' + displayObj.displayType + ' src="' + displayObj.uri + '" height="' + displayObj.height + '" width="' + displayObj.width + '">';
   console.log("buildHtml(): htmlString: " + htmlString);
   return htmlString;
 }
 
-var i=0;
-var firstTime = true;
 
 function hesDeadJim() {
   console.log("hesDeadJim() entry");
@@ -53,16 +51,19 @@ function hesDeadJim() {
 }
 
 
+var i=0;
+//var firstTime = true;  //see firstTime below to uncoment
 function advanceImage() { //used with setInterval
   var htmlString="";
   console.log("loopingPages.js: advanceImage(): value of HES_DEAD_JIM: " + HES_DEAD_JIM);
   if(HES_DEAD_JIM) {
     htmlString = hesDeadJim();  //always do this first
-  } else if(firstTime) { //then show splashPage
-      console.log("firstTime is true");
-      console.log("   calling buildHtml with sites.splashPage value: ", sites.splashPage);
-      htmlString=buildHtml(sites.splashPage);
-      firstTime=false;
+  // ==== uncomment the following if you want a special first page that only runs first time.
+  //} else if(firstTime) { //then show splashPage
+      //console.log("firstTime is true");
+      //console.log("   calling buildHtml with sites.splashPage value: ", sites.splashPage);
+      //htmlString=buildHtml(sites.splashPage);
+      //firstTime=false;
   } else if(pauseCheckboxValue==='Checked') {
       console.log("advanceImage(): pauseCheckboxValue === 'Checked'");
       console.log("returning to avoid advancing or redisplaying same page");
@@ -70,7 +71,7 @@ function advanceImage() { //used with setInterval
   } else {
     htmlString = buildHtml(sites.displayPages[i]);
     i++;
-    if(i==sites.length) {
+    if(i==sites.displayPages.length) {
       i=0;
     }
   }
